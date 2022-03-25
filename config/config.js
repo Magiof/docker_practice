00d91071a13db838b0600f7c420eb987da031212
seongwon <87584033+Magiof@users.noreply.github.com>
2022-03-26 01:44:24 +0900

    ADD: for test

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ module.exports = {
     "username": "root",
     "password": process.env.SQUARE_WITH_ME_DB_PASSWORD,
     "database": "Square_with_me_database",
-    "host": "nemo-test-mysql.cgxqbbyivfqr.ap-northeast-2.rds.amazonaws.com",
+    "host": "127.0.0.1",
     "port": 3306,
     "dialect": "mysql"
   },
